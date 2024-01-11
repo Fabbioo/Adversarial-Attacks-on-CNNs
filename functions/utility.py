603c9be11727f7322f17746b16a29ddd9644f56f
Fabbioo <fabio.mandalari@gmail.com>
2024-01-11 11:31:31 +0100

    ✅

--- functions/utility.py
+++ functions/utility.py
@@ -14,19 +14,23 @@ def visualize(image: torch.Tensor) -> None:
     plt.imshow(image)
 
 def normalize(image: torch.Tensor) -> torch.Tensor:
+    
     normalization = torchvision.transforms.Normalize(
         mean = [0.485, 0.456, 0.406],
         std = [0.229, 0.224, 0.225]
     )
     image = normalization(image)
+    
     return image
 
 def denormalize(image: torch.Tensor) -> torch.Tensor:
+    
     denormalization = torchvision.transforms.Normalize(
         mean = [-0.485/0.229, -0.456/0.224, -0.406/0.255],
         std = [1/0.229, 1/0.224, 1/0.255]
     )
     image = denormalization(image)
+    
     return image
 
 def preprocess(image: torch.Tensor, img_resize: tuple) -> torch.Tensor:
